3a679063bcb941cdd1fdb92f7f9b12854ae626b6
Diogo Borges de Moura <111078132+DigogSXD@users.noreply.github.com>
2024-06-04 09:11:08 -0300

    Create script.js

---
+++ codigos/Produtos/script.js
@@ -0,0 +1,7 @@
+document.getElementById('option1').addEventListener('click', function() {
+    alert('Você escolheu a Opção 1');
+});
+
+document.getElementById('option2').addEventListener('click', function() {
+    alert('Você escolheu a Opção 2');
+});
